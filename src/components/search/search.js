1754931e332e4c526795830e77ba79f5219d11a0
T2ayala <tonyayala02@gmail.com>
2022-08-24 18:55:35 -0500

    working on search function

---
+++ src/components/search/search.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import React from "react";
+
+
+const Search = ({onSearchChange}) => {
+
+    const [search, setSearch] = useState(null);
+ 
+    const handleOnChange =(searchData) =>{
+        setSearch(searchData);
+        onSearchChange(searchData);
+
+    }
+
+    return (
+     <AsyncPaginate
+     placeholder="Search for City"
+     debounceTimeout={600}
+     value={search}
+     onChange={handleOnChange}
+     />   
+    )
+}
+
+export default Search;
