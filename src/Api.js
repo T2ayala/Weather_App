28845af295ef883845f69bc23294c5451ef6e73a
T2ayala <tonyayala02@gmail.com>
2022-08-25 20:57:05 -0500

    done with search

--- src/Api.js
+++ src/Api.js
@@ -1,17 +1,12 @@
+import WEATHER_API_KEY from "./components/apikey";
+
 export const geoApiOptions = {
 
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
-  },
-
-
-    method: 'GET',
-	headers: {
-		
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
+    "X-RapidAPI-Key": WEATHER_API_KEY ,
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com"
+  }
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
