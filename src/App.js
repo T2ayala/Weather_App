7d0aba3605c3007f876bb03025622b54c87760bc
T2ayala <tonyayala02@gmail.com>
2022-09-01 19:32:59 -0500

    working on weather Icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import Search from "./components/search/search";
+import CurrentWeather from "./components/current-weather/current-weather";
+
 
 function App() {
   const handleOnSearchChange = (searchData) => {
@@ -9,6 +11,7 @@ function App() {
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
+      <CurrentWeather />
     </div>
   );
 }
