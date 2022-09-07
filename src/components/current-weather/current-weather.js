638ba121e7f0861283765e41cad638b9699e6f1c
T2ayala <tonyayala02@gmail.com>
2022-09-06 19:22:13 -0500

    created static data

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -4,12 +4,38 @@ const CurrentWeather = () => {
   return (
     <div className="weather">
       <div className="top">
-        <p className="city"> Dallas </p>
-        <p className="weather-description">Sunny</p>
+        <div>
+          <p className="city"> Dallas </p>
+          <p className="weather-description">Sunny</p>
+        </div>
+        <img alt="weather" className="weather-Icon" src="icons/01d.png" />
       </div>
-      <img alt="weather" className="weather-Icon" src=""/>
+      <div className="bottom">
+        <p className="temperature">100°F</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">104°F</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">15m/h</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">45%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">10hPa</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default CurrentWeather;
