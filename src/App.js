05e0a0b25ab5e476b6b81568028b96ced3933a7b
T2ayala <tonyayala02@gmail.com>
2022-09-19 18:19:11 -0500

    working onn current weather api

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import CurrentWeather from "./components/current-weather/current-weather";
 
 function App() {
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    const [lat, lon] = searchData.value.sp(" ");
+
+const CurrentWeatherFetch = fetch(`/weather?lat={lat}&lon={lon}&appid={API key}`)
+
   };
 
   return (
